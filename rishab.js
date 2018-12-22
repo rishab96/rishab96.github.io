bd87aafad1d8236a19abb55a0e56e8eab226625e
Rishab Mehra <rishabmeh3@gmail.com>
2018-12-22 15:54:58 -0800

    minor fix

--- rishab.js
+++ rishab.js
@@ -1,1 +1,1 @@
-Hi there. I am Clementine. I am Rishab's personal assistant. I can answer questions about Rishab. Whenever you ask me a question about him use his name. For example: What does Rishab like to eat? What does Rishab do?
+Hi there. I am Clementine. I am Rishab's personal assistant. 
